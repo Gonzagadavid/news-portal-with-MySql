6de8f2cecaed407ead81338da0891caabe8ce08d
Gonzagadavid <73129958+Gonzagadavid@users.noreply.github.com>
2021-02-28 09:57:50 -0300

    commit atual

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs')
 app.set('views', './app/views')
 
 app.use(express.static('./app/public'))
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 consign()
